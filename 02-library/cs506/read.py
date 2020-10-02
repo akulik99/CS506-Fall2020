d240fc43f8fe8bf3cf5fddf880fb792278c1b511
Albert Kulikowski <akulik@bu.edu>
2020-10-02 12:13:26 -0400

    adding git ignore files

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,18 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    matrix = []
+    with open(csv_file_path, "r") as f:
+        file = f.readlines()
+        for row in file:
+            row_strip = row.rstrip("\n")
+            split_rows = row_strip.split(",")
+            rows = []
+            for p in split_rows:
+                if p.isdigit():
+                    rows.append(int(p))
+                else:
+                    p_strip = p.strip("")
+                    rows.append(p_stripped)
+            matrix.append(rows)
+    return matrix
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,52 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    if (len(x)==0 or len(y)==0):
+        raise ValueError()
+    if len(x)!= len(y):
+        raise ValueError()
+    index = 0
+    sum_rest = 0
+    for index in range(len(x)):
+        e_dist = abs(x[index] - y[index]) ** 2
+        sum_rest += e_dist
+        index += 1
+    rest = sum_rest**(1/2)
+    return rest
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    if (len(x)==0 or len(y)==0):
+        raise ValueError()
+    if len(x)!=len(y):
+        raise ValueError()
+    index = 0
+    sum_rest = 0
+    for index in range(len(x)):
+        e_dist = abs(x[index] - y[index])
+        sum_rest += e_dist
+        index += 1
+    return sum_rest
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if (len(x)==0 or len(y)==0):
+        raise ValueError()
+    intersection = set(x).intersection(set(y))
+    union = set(x).union(set(y))
+    return 1 - (len(intersection)/len(union))
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
-
-# Feel free to add more
+    if (len(x)==0 or len(y)==0):
+        raise ValueError()
+    if len(x)!=len(y):
+        raise ValueError()
+    index = 0
+    numerator = 0
+    x_sum_e = 0
+    y_sum_e = 0
+    for feature in x:
+        e_product = x[index]*y[index]
+        numerator += e_product
+        x_sum_e += x[index]**2
+        y_sum_e += y[index]**2
+        index += 1
+    x_e_norm = x_sum_e**(1/2)
+    y_e_norm = y_sum_e**(1/2)
+    return numerator/(x_e*y_e_norm)
